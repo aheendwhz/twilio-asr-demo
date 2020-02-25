105481a8af29acd5d18a6ae79c611380864215df
Will Connor <will@babelforce.com>
2020-02-25 19:42:39 +0000

    update

--- demo.js
+++ demo.js
@@ -5,20 +5,23 @@ const r = require('request');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
  
 app.get('/', function (req, res) {
   
   const twiml = new VoiceResponse;
   
   const gather = twiml.gather({
-    action: 'https://asr-test.free.beeceptor.com/result',
+    action: '/thanks',
     method: 'POST',
     input: 'speech',
     language: 'en-US',
-    speechTimeout: 8,
+    speechTimeout: 4,
     speechModel: 'phone_call',
 
-  })
+  });
 
   gather.say('Tell me your deepest desires');
   
@@ -28,23 +31,36 @@ app.get('/', function (req, res) {
 });
 
 
-app.use(express.urlencoded());
-
-
-app.get('/thanks', function (req, res) {
+app.post('/thanks', function (req, res) {
   
   const postUrl = 'https://asr-test.free.beeceptor.com/result';
-
+  
   const response = new VoiceResponse;
-  response.say('Thanks for your input!');
 
-  //r.post({
-  //  uri: postUrl,
-     
-  //})
+  r.post({
+    uri: postUrl,
+    body: req.body,
+    json: true
+  });
+
+  response.say('Thanks for your input!');
 
   res.set('Content-Type', 'text/xml');
   res.end(response.toString());
-})
+});
+
+
+// echo test
+app.post('/echo', function (req, res) {
+
+  console.log('request:', req.body);
+
+  res.set('Content-Type', 'application/json');
+  res.status(200);
+  res.send(req.body);
+
+});
+
+
 
 app.listen(8080);
