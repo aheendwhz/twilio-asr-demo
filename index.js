0d6ff3ab9a50f2a799aba8fd808421c571e83765
Will Connor <will@babelforce.com>
2020-02-27 17:41:55 +0000

    added semi-realistic speech rec scenario for demo

--- index.js
+++ index.js
@@ -1,12 +1,93 @@
 
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const express = require('express');
-const r = require('request');
+const _ = require('lodash');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+// ASR config
+
+const hints = 'Ich möchte meine Anschrift ändern, Anschrift ändern, Anschrift, Anschriftenänderung';
+
+const keywords = [
+  'anschrift ändern',
+  'anschriftenänderung'
+];
+
+const voiceDefaults = {
+  voice: 'Polly.Marlene',
+  language: 'de-DE'
+};
+
+
+app.get('/asr', function (req, res) {
+  
+  const twiml = new VoiceResponse;
+  
+  const gather = twiml.gather({
+    action: '/thanks',
+    method: 'POST',
+    input: 'speech',
+    language: 'de-DE',
+    hints: hints,
+    speechModel: 'numbers_and_commands'
+
+  });
+
+  gather.say(voiceDefaults, 'Willkommen bei Eva Energie, wie kann ich Ihnen helfen?');
+  
+  // content type required by twilio
+  res.set('Content-Type', 'text/xml');
+  res.end(twiml.toString());
+});
+
+
+
+app.post('/thanks', function (req, res) {
+
+  const confidence = req.body.Confidence;
+  const speechResult = req.body.SpeechResult;
+
+  const speechRec = {
+    confidenceScore: confidence,
+    transcript: speechResult
+  }
+
+  const proc = x => _.split(_.toLower(x), ', ');
+  
+  const hintsProcessed = proc(hints);
+  const speechResultProcessed = proc(speechResult);
+
+  const matches = _.intersection(_.intersection(hintsProcessed, speechResultProcessed), keywords);
+
+  if (matches.length > 0) {
+    speechRec.keywordMatch = 'anschriftenaenderung'
+  } else {
+    speechRec.keywordMatch = 'no-match'
+  }
+
+  // speecRec is the object to use in the next step
+  // TODO concatenate query string for SIP request param:
+  // - x-babelforce-session-keyword --> `${speechRec.keywordMatch}`
+  // - x-babelforce-session-transcript --> url-stringify `${speechRec.transcript}`
+  // - x-babelforce-session-confidence --> transform float to low, medium, high
+  //   values, or string score out of 10?  
+  console.log(speechRec);
+
+
+  // confirmation message and end request
+  const response = new VoiceResponse;
+  response.say(voiceDefaults, 'Moment ich leite Sie gleich an einen Mitarbeiter, der Ihnen mit der Anschriftenänderung helfen kann, weiter');
+
+  res.set('Content-Type', 'text/xml');
+  res.end(response.toString());
+});
+
+
 
 
 
@@ -27,47 +108,7 @@ app.get('/siproute', function (req, res) {
 
 
 
-// ASR function
 
-app.get('/asr', function (req, res) {
-  
-  const twiml = new VoiceResponse;
-  
-  const gather = twiml.gather({
-    action: '/thanks',
-    method: 'POST',
-    input: 'speech',
-    language: 'en-US',
-    speechTimeout: 4,
-    speechModel: 'phone_call'
-
-  });
-
-  gather.say('Tell me your deepest desires');
-  
-  // content type required by twilio
-  res.set('Content-Type', 'text/xml');
-  res.end(twiml.toString());
-});
-
-
-app.post('/thanks', function (req, res) {
-  
-  const postUrl = 'https://asr-test.free.beeceptor.com/result';
-  
-  const response = new VoiceResponse;
-
-  r.post({
-    uri: postUrl,
-    body: req.body,
-    json: true
-  });
-
-  response.say('Thanks for your input!');
-
-  res.set('Content-Type', 'text/xml');
-  res.end(response.toString());
-});
 
 
 // echo test
@@ -80,7 +121,6 @@ app.post('/echo', function (req, res) {
   res.send(req.body);
 
 });
-
 
 
 app.listen(8080);
